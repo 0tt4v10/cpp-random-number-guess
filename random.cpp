7d658b071c6fcd8c2a5be89cb46c680df289940f
0tt4v10 <251075588+0tt4v10@users.noreply.github.com>
2026-06-21 22:40:43 +0200

    Add random number game

---
+++ random.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+
+int main() {
+    srand(time(0));
+    int secret = rand() % 100 + 1;
+    int guess = 0;
+    int tries = 0;
+
+    std::cout << "Welcome to Random Number Game (1-100)\n\n";
+
+    while (guess != secret) {
+        std::cout << "Your tipp: ";
+        std::cin >> guess;
+        tries++;
+
+        if (guess < secret) {
+            std::cout << "Too low!\n";
+        }
+        else if (guess > secret) {
+            std::cout << "Too High!\n";
+        }
+        else {
+            std::cout << "Correct! Tries: " << tries;
+        }
+    }
+    std::cin.ignore();
+    std::cin.get();
+    return 0;
+}
